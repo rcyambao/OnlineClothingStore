baffbec2e32041201777bc2afa0e6a4e1b7dad76
rcyambao <78418175+rcyambao@users.noreply.github.com>
2021-05-20 23:09:28 +0800

    login,register layout

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,60 +1,60 @@
+
+
+
 <x-guest-layout>
-    <x-jet-authentication-card>
-        <x-slot name="logo">
-            <x-jet-authentication-card-logo />
-        </x-slot>
 
-        <x-jet-validation-errors class="mb-4" />
+<main id="main" class="main-site left-sidebar">
 
-        <form method="POST" action="{{ route('register') }}">
-            @csrf
+		<div class="container">
 
-            <div>
-                <x-jet-label for="name" value="{{ __('Name') }}" />
-                <x-jet-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
-            </div>
+			<div class="wrap-breadcrumb">
+				<ul>
+					<li class="item-link"><a href="/" class="link">home</a></li>
+					<li class="item-link"><span>Register</span></li>
+				</ul>
+			</div>
+			<div class="row">
+				<div class="col-lg-6 col-sm-6 col-md-6 col-xs-12 col-md-offset-3">							
+					<div class=" main-content-area">
+						<div class="wrap-login-item ">
+							<div class="register-form form-item ">
+                                <x-jet-validation-errors class="mb-4" />
+								<form class="form-stl" action="{{route('register')}}" name="frm-login" method="POST" >
+                                    @csrf
+									<fieldset class="wrap-title">
+										<h3 class="form-title">Create an account</h3>
+										<h4 class="form-subtitle">Personal infomation</h4>
+									</fieldset>									
+									<fieldset class="wrap-input">
+										<label for="frm-reg-lname">Name</label>
+										<input type="text" id="frm-reg-lname" name="name" placeholder="Your Name" :value="name"required autofocus autocomplete="name">
+									</fieldset>
+									<fieldset class="wrap-input">
+										<label for="frm-reg-email">Email Address</label>
+										<input type="email" id="frm-reg-email" name="email" placeholder="Email address" :value="email">
+									</fieldset>
 
-            <div class="mt-4">
-                <x-jet-label for="email" value="{{ __('Email') }}" />
-                <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
-            </div>
+                                    <fieldset>
+                                        <div class="mt-4">
+                                            <x-jet-label for="password" value="{{ __('Password') }}" />
+                                            <x-jet-input id="password" class="block mt-1 w-full" type="password" placeholder="Password" name="password" required autocomplete="new-password" />
+                                        </div>
 
-            <div class="mt-4">
-                <x-jet-label for="password" value="{{ __('Password') }}" />
-                <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
-            </div>
+                                        <div class="mt-4">
+                                            <x-jet-label for="password_confirmation" value="{{ __('Confirm Password') }}" />
+                                            <x-jet-input id="password_confirmation" class="block mt-1 w-full" type="password" placeholder="Confirm Password" name="password_confirmation" required autocomplete="new-password" />
+                                        </div>
+                                    </fieldset>
+									
+									<input type="submit" class="btn btn-sign" value="Register" name="register">
+								</form>
+							</div>											
+						</div>
+					</div><!--end main products area-->		
+				</div>
+			</div><!--end row-->
 
-            <div class="mt-4">
-                <x-jet-label for="password_confirmation" value="{{ __('Confirm Password') }}" />
-                <x-jet-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" required autocomplete="new-password" />
-            </div>
+		</div><!--end container-->
 
-            @if (Laravel\Jetstream\Jetstream::hasTermsAndPrivacyPolicyFeature())
-                <div class="mt-4">
-                    <x-jet-label for="terms">
-                        <div class="flex items-center">
-                            <x-jet-checkbox name="terms" id="terms"/>
-
-                            <div class="ml-2">
-                                {!! __('I agree to the :terms_of_service and :privacy_policy', [
-                                        'terms_of_service' => '<a target="_blank" href="'.route('terms.show').'" class="underline text-sm text-gray-600 hover:text-gray-900">'.__('Terms of Service').'</a>',
-                                        'privacy_policy' => '<a target="_blank" href="'.route('policy.show').'" class="underline text-sm text-gray-600 hover:text-gray-900">'.__('Privacy Policy').'</a>',
-                                ]) !!}
-                            </div>
-                        </div>
-                    </x-jet-label>
-                </div>
-            @endif
-
-            <div class="flex items-center justify-end mt-4">
-                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
-                    {{ __('Already registered?') }}
-                </a>
-
-                <x-jet-button class="ml-4">
-                    {{ __('Register') }}
-                </x-jet-button>
-            </div>
-        </form>
-    </x-jet-authentication-card>
+	</main>
 </x-guest-layout>
