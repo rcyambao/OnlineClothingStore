71fe8503985af6441ed16ab9c333877febcedbe1
rcyambao <78418175+rcyambao@users.noreply.github.com>
2021-05-21 21:50:54 +0800

    products

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -185,7 +185,7 @@
 						<div class="container">
 							<ul class="nav primary clone-main-menu" id="mercado_main" data-menuname="Main menu" >
 								<li class="menu-item home-icon">
-									<a href="index.html" class="link-term mercado-item-title"><i class="fa fa-home" aria-hidden="true"></i></a>
+									<a href="/" class="link-term mercado-item-title"><i class="fa fa-home" aria-hidden="true"></i></a>
 								</li>
 								<li class="menu-item">
 									<a href="/" class="link-term mercado-item-title">About Us</a>
