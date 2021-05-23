8d4f9ba3bcc4cdf10679ccfcccf96384a36b3089
rcyambao <78418175+rcyambao@users.noreply.github.com>
2021-05-23 18:53:08 +0800

    search

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use App\Http\Livewire\CartComponent;
 use App\Http\Livewire\CheckoutComponent;
 use App\Http\Livewire\DetailsComponent;
 use App\Http\Livewire\CategoryComponent;
+use App\Http\Livewire\SearchComponent;
 // use App\Http\Livewire\Product;
 use App\Http\Livewire\User\UserDashboardComponent;
 use App\Http\Livewire\Admin\AdminDashboardComponent;
@@ -33,7 +34,8 @@ Route::get('/cart', CartComponent::class)->name('product.cart');
 Route::get('/checkout', CheckoutComponent::class);
 Route::get('/product/{slug}', DetailsComponent::class)->name('product.details');
 Route::get('/product-category/{category_slug}', CategoryComponent::class)->name('product.category');
-
+Route::get('/search', SearchComponent::class)->name('product.search');
+ 
 // Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
 //     return view('dashboard');
 // })->name('dashboard');
