2092e8bd750d756a522c35f0867a67f4b9564e52
DianneSudario <201810784@gordoncollege.edu.ph>
2021-05-28 20:05:33 +0800

    delete products-admin

--- app/Http/Livewire/Admin/AdminProductComponent.php
+++ app/Http/Livewire/Admin/AdminProductComponent.php
@@ -9,6 +9,14 @@ use App\Models\Product;
 class AdminProductComponent extends Component
 {
     use WithPagination;
+
+    public function deleteProduct($id)
+    {
+        $product = Product::find($id);
+        $product->delete();
+        session()->flash('message', 'PRODUCT HAS BEEN DELETED SUCCESSFULLY!');
+    }
+
     public function render()
     {
         $products = Product::paginate(10);
